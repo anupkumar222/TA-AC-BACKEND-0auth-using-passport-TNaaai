f35305e07320826ff4ae664ba7006885fef2d556
anup kumar <kanup414@gmail.com>
2023-01-16 23:56:25 +0530

    done

--- block-BNaaed/passport/app.js
+++ block-BNaaed/passport/app.js
@@ -3,11 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo');
+var passport = require('passport');
+
+//config .env file
+require('dotenv').config();
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// connect mongoose
+mongoose.connect('mongodb://localhost/passport', (err) => {
+  console.log("connected", err ? false : true );
+});
+
+require('./modules/passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +33,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/passport' }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ block-BNaaed/passport/modules/passport.js
@@ -0,0 +1,10 @@
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy; 
+
+passport.use(new GitHubStrategy({
+    clientID : process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'auth/github/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+}))
